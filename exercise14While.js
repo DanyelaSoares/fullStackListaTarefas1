526fb926cd65fde3ccc78b0b52a8f9ae858bdc49
Daniela Soares <112218335+DanyelaSoares@users.noreply.github.com>
2024-08-25 23:35:26 -0300

    Exercise14 - If Else / Do While
    Certifique-se de que o Node.js está instalado. 
    Este código utiliza o módulo prompt-sync para capturar a entrada do usuário. Para instalar este módulo, siga os passos abaixo:
    Abra o terminal no diretório onde o arquivo .js está salvo.
    Execute o comando para instalar o prompt-sync: npm install prompt-sync
    Abra o terminal no diretório onde o arquivo .js está salvo. Se estiver usando o VS Code, você pode abrir o terminal integrado.
    
    Ao executar o código, o programa solicitará ao usuário que forneça um número inteiro para calcular o fatorial.
    O programa calculará o fatorial desse número utilizando um loop while e exibirá o resultado no console.
    Se o número for negativo, o programa exibirá uma mensagem informando que o fatorial não é definido para números negativos.

---
+++ exercise14While.js
@@ -0,0 +1,30 @@
+
+const prompt = require('prompt-sync')();
+
+let number = parseInt(prompt('Digite um número inteiro para calcular o fatorial: '));
+let factorial = 1;
+let i = 1;
+
+if (number < 0) {
+    console.log('O fatorial não é definido para números negativos.');
+} else {
+    while (i <= number) {
+        factorial *= i;  // Multiplica o valor atual de factorial por i
+        i++;             // Incrementa i
+    }
+    console.log(`O fatorial de ${number} é: ${factorial}`);
+}
+
+
+/*
+Certifique-se de que o Node.js está instalado. 
+Este código utiliza o módulo prompt-sync para capturar a entrada do usuário. Para instalar este módulo, siga os passos abaixo:
+Abra o terminal no diretório onde o arquivo .js está salvo.
+Execute o comando para instalar o prompt-sync: npm install prompt-sync
+Abra o terminal no diretório onde o arquivo .js está salvo. Se estiver usando o VS Code, você pode abrir o terminal integrado.
+
+Ao executar o código, o programa solicitará ao usuário que forneça um número inteiro para calcular o fatorial.
+O programa calculará o fatorial desse número utilizando um loop while e exibirá o resultado no console.
+Se o número for negativo, o programa exibirá uma mensagem informando que o fatorial não é definido para números negativos.
+
+*/
